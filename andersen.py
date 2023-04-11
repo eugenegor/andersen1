bd1fa8ef066878982c622efe1474a90d20b7419f
eugenegor <98324507+eugenegor@users.noreply.github.com>
2023-04-12 01:42:11 +0200

    Initial commit

---
+++ andersen.py
@@ -0,0 +1,17 @@
+
+
+num = int(input("Введите число: "))
+if num > 7:
+    print("Привет")
+
+name = input("Введите имя: ")
+if name == "Вячеслав":
+    print("Привет, Вячеслав")
+else:
+    print("Нет такого имени")
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+for num in arr:
+    if num % 3 == 0:
+        print(num)
+
